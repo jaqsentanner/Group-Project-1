76f7ca7008b264a53d0e6c20d590a9d02902afb0
unknown <jtmeyer2014@gmail.com>
2022-10-11 22:35:15 -0400

    updated news testing

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var news1 = document.getElementById("news1")
-var news2 = document.getElementById("news2")
-var news3 = document.getElementById("news3")
-var requestURL = 'https://api.thenewsapi.com/v1/news/top?api_token=1JHhYSZpbVMto5AgJ58VUygKJEFfTJrArZBoUqWC'
+var header1 = document.getElementById("header1");
+var body1 = document.getElementById("body1");
+var url1 = document.getElementById("url1");
+var header2 = document.getElementById("header2");
+var body2 = document.getElementById("body2");
+var url2 = document.getElementById("url2");
+var header3 = document.getElementById("header3");
+var body3 = document.getElementById("body3");
+var url3 = document.getElementById("url3");
+
+var requestURL = 'https://api.thenewsapi.com/v1/news/top?api_token=1JHhYSZpbVMto5AgJ58VUygKJEFfTJrArZBoUqWC&language=en'
 
 function newsAPI () {
     fetch (requestURL) 
@@ -12,6 +19,20 @@ function newsAPI () {
 
     .then (function (data) {
         console.log(data);
+        console.log(data.data[0].title);
+        console.log(data.data[0].description);
+        console.log(data.data[0].url);
+        header1.textContent = data.data[0].title
+        body1.textContent = data.data[0].description
+        url1.textContent = data.data[0].url
+        header2.textContent = data.data[1].title
+        body2.textContent = data.data[1].description
+        url2.textContent = data.data[1].url
+        header3.textContent = data.data[2].title
+        body3.textContent = data.data[2].description
+        url3.textContent = data.data[2].url
+        
+        
                 
     })
 }
